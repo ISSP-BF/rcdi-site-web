20e76440b0a35a13bab557ea37bfa19237fd0f36
DAO Hamadou <daohamadou@gmail.com>
2023-03-15 10:13:56 +0000

    Mise a jour et changement de template

---
+++ wp-content/plugins/spice-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'b961592dab067df0c927');
